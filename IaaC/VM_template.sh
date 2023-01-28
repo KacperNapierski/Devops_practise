a6e1ab84c94607dbee2d8514733dd757891bffac
KacperNapierski <kacper.t.napierski@gmail.com>
2023-01-28 17:51:57 +0100

    Terraform error 400 + VM template automation

---
+++ IaaC/VM_template.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+get_basic_vm_settings(){
+    read -p 'VM ID: ' vm_id
+    read -p 'VM Name: ' vm_name
+    read -p 'VM memory: ' vm_memory
+    read -p 'VM cores: ' vm_core
+}
+
+#---------Requirements------------
+rm focal-server-cloudimg-amd64.img
+wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
+apt update -y
+apt install libguestfs-tools -y
+echo 'Requirements downloaded'
+
+get_basic_vm_settings
+
+#---------Template creation-----------
+qm create $vm_id --name $vm_name --memory $vm_memory --cores $vm_core --net0 virtio,bridge=vmbr0
+qm importdisk $vm_id focal-server-cloudimg-amd64.img local
+qm set $vm_id --scsihw virtio-scsi-pci --scsi0 local:vm-$vm_id-disk-0
+qm set $vm_id --boot c --bootdisk scsi0
+qm set $vm_id --ide2 local:cloudinit
+qm set $vm_id --serial0 socket --vga serial0
+qm set $vm_id --agent enabled=1
+qm template $vm_id
+rm focal-server-cloudimg-amd64.img
+echo 'Template created'
--- IaaC/worker-node.tf
+++ IaaC/worker-node.tf
@@ -1,27 +1,41 @@
 resource "proxmox_vm_qemu" "worker_node" {
 
-    target_node = "production"
-    count = 2
-    name = "worker-node-${count.index + 1}"
-    vmid = "30${count.index + 1}"
-    desc = "Ubuntu Server based worker VM"
-    iso = "ubuntu-22.04.1-live-server-amd64.iso"
-    onboot = true
+  target_node = "production"
+  clone       = "ubuntu-2004-cloudinit-template"
+  count       = 2
+  name        = "worker-node-${count.index + 1}"
+  vmid        = "30${count.index + 1}"
+  #desc        = "Ubuntu Server based worker VM"
 
-    memory = 1024
-    sockets = 1
-    cores = 1
-    scsihw = "virtio-scsi-single"
-    #ipconfig0 = "ip = 192.168.0.15${count.index + 1}/24, gw = 192.168.0.1"
+  os_type   = "cloud-init"
+  onboot    = true
+  agent     = 1
+  memory    = 1024
+  sockets   = 1
+  cores     = 1
+  scsihw    = "virtio-scsi-pci"
+  bootdisk  = "scsi0"
+  ipconfig0 = "ip = 192.168.0.15${count.index + 1}/24, gw = 192.168.0.1"
 
-    network {
-        model = "virtio"
-        bridge = "vmbro"
-    }
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
 
-    disk {
-        type = "scsi"
-        storage = "local-lvm"
-        size = "20G"
-    }
+  disk {
+    type     = "scsi"
+    storage  = "local-lvm"
+    size     = "20G"
+    iothread = 1
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+
+  sshkeys = <<EOF
+  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDXY5wKp6+K4b3u+QOIc3x+O4L4TuEiCc/DT5qMZaBaHcQhsp5/Jb8GQU1/zftE8uUOq/gmM8qQvvBRlpzsRGiYYoSCuog0bVd9j4srArhDoPXXFAvKwj3Nwnt7Adif9mq4H9emjcxqs3sa6T1PvN2+RqmkihBEcTPGinWKNYpKCt2FoGBGyELDrpXTr72pEaSW+LKfigokrA53AdqCnlgB40C+kc/ehVt01VGMZ0TXnrMGb2P3Fe5U/4YFyUcHLYMAFszSzjqYhiUvdMUhHmOyfBwP7bGDHDhi1d5t7D6OMolN/+nfwlZFEMVuZjxmCxypE0UEyqg1xUMgq/wM0636As8ZiDA7zIXrucGaHEnDcyVVtGqFwtE0uDCZ2HZba8y9eaf8Zrx+nt/WA6IMfqsQdwA6wiTUv+gKLgaF6CytUbYn7DOJvPCifx0Cu/344nLWhuLgw6ppptRkAniaYANGXDMONHqIaEWtRMcK5dIUeoTXHc38acASMWcsAhWFjRM= root@production
+  EOF
 }
