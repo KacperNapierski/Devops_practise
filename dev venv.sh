36ecebfa994faa4e50bc30bae4c1ab7200297405
KacperNapierski <kacper.t.napierski@gmail.com>
2023-05-30 15:16:17 +0200

    Gitlab runners registers and contact gitlab.com

--- dev venv.sh
+++ dev venv.sh
@@ -3,5 +3,7 @@
 #preparing dev-venv in fedora os
 
 sudo dnf install -y dnf-plugins-core
-sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/$release/hashicorp.repo
+sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
 sudo dnf install terraform
+python3 -m pip install --user ansible
+python3 -m pip install --upgrade --user ansible
